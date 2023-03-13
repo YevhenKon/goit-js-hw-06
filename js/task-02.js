c70a7596df68d7049a1821adcf1c8a0c5f27f268
Yevhen Kontselenko <105706808+YevhenKon@users.noreply.github.com>
2023-03-13 21:01:51 +0200

    2 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const itemIngredients = ingredients.map(vegetable => {
+  const newItem = document.createElement("li")
+  newItem.textContent = vegetable
+  newItem.classList.add("item")
+
+  console.log(newItem);
+  return newItem
+})
+
+console.log( itemIngredients);
+
+const list = document.querySelector('#ingredients')
+
+const itemList = list.prepend(...itemIngredients)

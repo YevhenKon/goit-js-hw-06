83003e3a882ff5d7a31e948d66600f61aec84e81
Yevhen Kontselenko <105706808+YevhenKon@users.noreply.github.com>
2023-03-16 20:28:42 +0200

    9 task

--- js/task-07.js
+++ js/task-07.js
@@ -6,6 +6,5 @@ const changeText = document.querySelector('#text')
 sizeText.addEventListener('input', (evt) => {
 
     changeText.style.fontSize = `${evt.target.value}px`
-console.log(Number(evt.target.value));
 
 })
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const bodyColor = document.querySelector('body')
+
+const btnChangeColor = document.querySelector('button')
+
+const colors = document.querySelector('span')
+
+btnChangeColor.addEventListener('click', () => {
+  const newColor = getRandomHexColor()
+  bodyColor.style.backgroundColor = newColor
+  colors.textContent = newColor
+})

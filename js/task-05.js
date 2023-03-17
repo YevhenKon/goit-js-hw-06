4804be8c26da3969f4d43cd3d855577f8292bf9c
Yevhen Kontselenko <105706808+YevhenKon@users.noreply.github.com>
2023-03-17 15:10:32 +0200

    5 task chenge

--- js/task-05.js
+++ js/task-05.js
@@ -5,5 +5,12 @@ const outputText = document.querySelector('#name-output')
 
 
 inputText.addEventListener('input', (evt) => {
+    console.log(evt.currentTarget);
+    if (evt.currentTarget.value === '') {
+
+        outputText.textContent = 'Anonymous'
+        console.log(outputText);
+        return
+    }
     outputText.textContent = evt.currentTarget.value
 })
